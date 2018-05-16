0136587d4456b0d99d49c0b28254017eae769b53
hejiyan <hejiyan@mail.ustc.edu.cn>
2018-05-16 15:21:21 +0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+mkdir fs
+./vtfs fs
+cd fs
+
+# standard test
+ls -al
+echo helloworld > testfile
+ls -l testfile
+cat testfile
+dd if=/dev/zero of=testfile bs=1M count=200
+ls -l testfile
+dd if=/dev/urandom of=testfile bs=1M count=1 seek=10
+ls -l testfile
+dd if=testfile of=/dev/null
+rm testfile
+ls -al
+
+# dir test
+
+mkdir dir1 dir2 dir3
+echo aaa > dir1/aaa
+echo bbb > dir2/bbb
+echo ccc > dir3/ccc
+ls -al
+cat dir1/aaa
+cat dir2/bbb
+cat dir3/ccc
+
+cd ..
+sudo umount fs
+rm -rf fs
